3be94b3a310d7dbf15ae2d797557afd05731ac79
Bishal Wagle <bishalw@gmail.com>
2022-08-23 15:04:44 -0700

    added CoinImageService and CoinImageView

---
+++ Crypter/Core/Components/Coinimage/CoinImageView.swift
@@ -0,0 +1,48 @@
+//
+//  CoinImageView.swift
+//  Crypter
+//
+//  Created by Bishalw on 8/23/22.
+//
+
+import SwiftUI
+
+class CoinImageViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    init(){
+        getImage()
+    }
+    
+    private func getImage(){
+        
+    }
+}
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView()
+    }
+}
---
+++ Crypter/Services/CoinImageService.swift
@@ -0,0 +1,21 @@
+//
+//  CoinImageService.swift
+//  Crypter
+//
+//  Created by Bishalw on 8/23/22.
+//
+
+import Foundation
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    
+    init() {
+        getCoinImage()
+        
+    }
+    private func getCoinImage() {
+        
+    }
+}
