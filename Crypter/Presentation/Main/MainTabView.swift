9d074b6388c8a3dc7af62430c15074d7e2904d08
Bishal Wagle <bishalw@gmail.com>
2024-07-04 23:39:20 -0700

    updated tab bar appearance

--- Crypter/Presentation/Main/MainTabView.swift
+++ Crypter/Presentation/Main/MainTabView.swift
@@ -22,6 +22,14 @@ struct MainTabView: View {
                     .tabItem {
                         Label("Portfolio", systemImage: "creditcard")
                     }
+            }.onAppear {
+                let appearance = UITabBarAppearance()
+                  appearance.configureWithOpaqueBackground()
+                  UITabBar.appearance().standardAppearance = appearance
+                  
+                  if #available(iOS 15.0, *) {
+                      UITabBar.appearance().scrollEdgeAppearance = appearance
+                  }
             }
         }
       
