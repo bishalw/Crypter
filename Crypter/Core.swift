5903f63c0a7b8662be7c24b1dcae3d5d72bbe13c
Bishal Wagle <bishalw@gmail.com>
2026-04-02 05:48:38 -0700

    (chore) - clean up extraneous whitespaces in core

--- Crypter/Core.swift
+++ Crypter/Core.swift
@@ -9,31 +9,31 @@ import Foundation
 
 class Core: ObservableObject {
     
-    private (set) lazy var networkingManager: NetworkingManager = {
+    private(set) lazy var networkingManager: NetworkingManager = {
         return NetworkingManagerImpl()
     }()
     
-    private (set) lazy var coinAPIService: CoinAPIService = {
+    private(set) lazy var coinAPIService: CoinAPIService = {
         return CoinAPIServiceImpl(networkingManager: self.networkingManager)
     }()
     
-    private (set) lazy var localFileManager: LocalFileManager = {
+    private(set) lazy var localFileManager: LocalFileManager = {
         return LocalFileManagerImpl()
     }()
     
-    private (set) lazy var coinImageRepository: CoinImageRepository = {
+    private(set) lazy var coinImageRepository: CoinImageRepository = {
         return CoinImageRepositoryImpl(networkingManager: self.networkingManager, localFileManager: self.localFileManager)
     }()
     
-    private (set) lazy var globalAPIService: GlobalAPIService = {
+    private(set) lazy var globalAPIService: GlobalAPIService = {
         return GlobalAPIServiceImpl(networkingManager: self.networkingManager)
     }()
     
-    private (set) lazy var cryptoRepository: CryptoRepository = {
+    private(set) lazy var cryptoRepository: CryptoRepository = {
         return CryptoRepositoryImpl(coinAPIService: self.coinAPIService, globalAPIService: self.globalAPIService)
     }()
     
-    private (set) lazy var cryptoStore: CryptoStore = {
+    private(set) lazy var cryptoStore: CryptoStore = {
         return CryptoStoreImpl(repository: self.cryptoRepository)
     }()
 }
